7e7a82b17b345039fe398758cfcb8cefe931cbaf
Den Ermol <denisermolenko17@gmail.com>
2022-06-02 23:11:54 +0300

    fixed testing

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  preset: "ts-jest",
-  globals: {},
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.vue$": "@vue/vue3-jest",
-    "^.+\\js$": "babel-jest",
-  },
-  moduleFileExtensions: ["vue", "js", "json", "jsx", "ts", "tsx", "node"],
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
 };
